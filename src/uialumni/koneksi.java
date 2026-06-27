2bb37c338c133c39771db541f35fc32d5d9e5183
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-06-27 11:51:28 +0700

    koneksi ke dtaabase

---
+++ src/uialumni/koneksi.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uialumni;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ACER
+ */
+public class koneksi {
+    
+    private static Connection mysqlconfiq;
+    
+    public static Connection konek (){
+        try {
+            
+            String url = "jdbc:mysql://localhost:3306/alumni_202557201044";
+            
+            String user = "root";
+            
+            String pass = "";
+            
+            mysqlconfiq = DriverManager.getConnection(url, user, pass);
+            
+        } catch (SQLException sQLException) {
+            System.err.println(sQLException.getMessage());
+        }
+        return mysqlconfiq;
+    }
+    
+}
